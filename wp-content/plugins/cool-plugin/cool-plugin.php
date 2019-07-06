ab0823e51fbb9429e53f56fa600fb42496ab48f7
Peter Suhm <peter@suhm.dk>
2019-07-06 08:26:39 +0200

    add cool plugin

---
+++ wp-content/plugins/cool-plugin/cool-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package Cool_Plugin
+ */
+/*
+Plugin Name: Cool Plugin
+Description: A cool plugin that's pretty cool.
+Author: Peter Suhm
+Version: 1.3.3.7
+Author URI: https://branchci.com
+*/
